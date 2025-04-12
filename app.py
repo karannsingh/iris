12cb64212e9aa15a2ada31587366db5cea2c656d
Singh Karan Narendra <73518312+karannsingh@users.noreply.github.com>
2025-04-12 17:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -153,5 +153,5 @@ def verify_with_stored_data():
     except Exception as e:
         return jsonify({"status": "error", "message": f"Verification error: {str(e)}"})        
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
